d87b8e2a9ccb577a5a10bccc3693d571b62a83a7
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-11 16:03:07 +0530

    [Refactor] Tictactoe game with deciding other winners

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -16,8 +16,23 @@ class TicTacToeTest {
         player2.play(board, 2, 2);
         player1.play(board, 1, 3);
 
-        System.out.println(game.findWinner());
-
         assertEquals("Player 1 is Winner", game.findWinner());
+    }
+
+    @Test
+    void shouldReturnPlayer2IsWinner() {
+        Board board = new Board();
+        TicTacToe game = new TicTacToe(board);
+        Player player1 = game.createPlayer1();
+        Player player2 = game.createPlayer2();
+
+        player1.play(board, 1, 1);
+        player2.play(board, 2, 1);
+        player1.play(board, 1, 2);
+        player2.play(board, 2, 2);
+        player1.play(board, 3, 3);
+        player2.play(board, 2, 3);
+
+        assertEquals("Player 2 is Winner", game.findWinner());
     }
 }
