d44ff3fb3f4632f46be33829c21736c15ce0356d
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-11 16:06:16 +0530

    [Refactor] Tictactoe game with decide if there is no winner

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -35,4 +35,24 @@ class TicTacToeTest {
 
         assertEquals("Player 2 is Winner", game.findWinner());
     }
+
+    @Test
+    void shouldReturnNoOneIsWinner() {
+        Board board = new Board();
+        TicTacToe game = new TicTacToe(board);
+        Player player1 = game.createPlayer1();
+        Player player2 = game.createPlayer2();
+
+        player1.play(board, 1, 2);
+        player2.play(board, 1, 1);
+        player1.play(board, 2, 1);
+        player2.play(board, 1, 3);
+        player1.play(board, 2, 3);
+        player2.play(board, 2, 2);
+        player1.play(board, 3, 1);
+        player2.play(board, 3, 3);
+        player1.play(board, 3, 2);
+
+        assertEquals("No Winner", game.findWinner());
+    }
 }
